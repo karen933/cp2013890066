8a133407d5f6e548512d3e653f014f32fc2d4290
2013890066 <harryjames@naver.com>
2018-11-06 17:42:40 +0900

    '20181106포인터'

---
+++ 20181106/20181106swap/swap.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void swap(int* ptr1, int* ptr2){
+	int temp;
+	temp=*ptr1;
+	*ptr1=*ptr2;
+	*ptr2=temp;
+}
+int main(){
+	int a=10, b=20;
+	//swap : 변수 a,b의 값을 바꾼다
+	swap(&a,&b);
+	printf("a=%d, b=%d \n",a,b);
+
+	return 0;
+}
